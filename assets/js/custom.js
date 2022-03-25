5eb969b749d30ef4088857c1457cae23a919ae8f
iamsiraj13 <iamsiraj13@gmail.com>
2022-03-25 10:54:05 +0600

    initialize

---
+++ assets/js/custom.js
@@ -0,0 +1,18 @@
+ var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+} 
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
